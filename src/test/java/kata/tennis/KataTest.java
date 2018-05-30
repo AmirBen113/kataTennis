c34bcd65e895d4d6882d4d8a45361b770457a98b
AmirBen113 <39623134+AmirBen113@users.noreply.github.com>
2018-05-30 16:30:54 +0200

    Add files via upload
    implements some tests

--- src/test/java/kata/tennis/KataTest.java
+++ src/test/java/kata/tennis/KataTest.java
@@ -1,9 +1,14 @@
 package kata.tennis;
 
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import kata.tennis.constants.Constants;
+import kata.tennis.enums.MatchStatus;
 import kata.tennis.factory.MatchFactory;
 
 public class KataTest {
@@ -25,7 +30,9 @@ public class KataTest {
 	 */
 	@Test
 	public void fifteenShouldBeThePlayer1Score() {
-
+		Game game = match.getCurrentGame();
+		match.getCurrentGame().scorePoint(player1);
+		assertThat(game, hasProperty("status", is("15-0")));
 	}
 	
 	/**
@@ -33,20 +40,44 @@ public class KataTest {
 	 */
 	@Test
 	public void fifteenShouldBeThePlayer2Score() {
-
+		Game game = match.getCurrentGame();
+		match.getCurrentGame().scorePoint(player2);
+		assertThat(game, hasProperty("status", is("0-15")));
 	}
 	
 	// test ex 1 (6 -1) (7-5) (1-0)
 	@Test
 	public void inProgressStatusShouldBeTheMatchStatus() {
-
+		
+		// init test
+		initTest();
+		winNormalGame(player1);
+		// game status : 15-30
+		match.getCurrentGame().scorePoint(player2);
+		match.getCurrentGame().scorePoint(player1);
+		match.getCurrentGame().scorePoint(player2);
+		assertThat(match, hasProperty("score", is("(6-1) (7-5) (1-0)")));
+		assertThat(match, hasProperty("status", is(MatchStatus.IN_PROGRESS)));
+		assertThat(match.getCurrentGame(), hasProperty("status", is("15-30")));
 	}
 
 	// test ex 2 (6 -1) (7-5) (0-0)
 	@Test
 	public void deuceShouldBeTheCurrentGameStatusAtLeastThreePointsHaveBeenScoredByPlayersEqual() {
-	
-
+		// init test
+		initTest();
+		
+		// game status : deuce
+		for (int i = 0; i <= 2 ; i++) 
+			match.getCurrentGame().scorePoint(player2);
+		
+		for (int i = 0; i <= 2 ; i++) 
+			match.getCurrentGame().scorePoint(player1);
+		
+		
+		assertThat(match, hasProperty("score", is("(6-1) (7-5) (0-0)")));
+		assertThat(match, hasProperty("status", is(MatchStatus.IN_PROGRESS)));
+		assertThat(match.getCurrentGame(), hasProperty("status", is("deuce")));
 	}
 
 
